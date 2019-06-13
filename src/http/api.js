2d094a416b5e0cfefc665de06337fa4a85f24021
ciki6 <18694942447@163.com>
2019-06-14 00:42:54 +0800

    first commit

---
+++ src/http/api.js
@@ -0,0 +1,16 @@
+/* 
+ * 接口统一集成模块
+ */
+import * as login from './modules/login'
+import * as user from './modules/user'
+import * as dept from './modules/dept'
+import * as menu from './modules/menu'
+
+
+// 默认全部导出
+export default {
+    login,
+    user,
+    dept,
+    menu
+}
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import api from './http'
+import i18n from './i18n'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+require('./mock')
+
+Vue.use(ElementUI)
+Vue.use(api)
+
+new Vue({
+  el: '#app',
+  i18n,
+  router,
+  store,
+  render: h => h(App)
+});
