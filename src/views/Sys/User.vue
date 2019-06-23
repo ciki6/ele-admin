bb4ab41ba05ef9b1a6d3b24d1a37fbc139cb2825
ciki6 <18694942447@163.com>
2019-06-24 00:00:44 +0800

    用户列表过滤

--- src/views/Sys/User.vue
+++ src/views/Sys/User.vue
@@ -37,7 +37,7 @@
 	</div>
 	<!--表格内容栏-->
 	<bit-table permsEdit="sys:user:edit" permsDelete="sys:user:delete"
-		:data="pageResult" :columns="filterColumns.length > 0 ? filterColumns : columns"
+		:data="pageResult" :columns="filterColumns"
 		@findPage="findPage" @handleEdit="handleEdit" @handleDelete="handleDelete">
 	</bit-table>
 	<!--新增编辑界面-->
@@ -106,19 +106,7 @@ export default {
 			filters: {
 				name: ''
 			},
-			columns: [
-				{prop:"id", label:"ID", minWidth:50},
-				{prop:"name", label:"用户名", minWidth:120},
-				{prop:"deptName", label:"机构", minWidth:120},
-				{prop:"roleNames", label:"角色", minWidth:100},
-				{prop:"email", label:"邮箱", minWidth:120},
-				{prop:"mobile", label:"手机", minWidth:100},
-				{prop:"status", label:"状态", minWidth:70},
-				// {prop:"createBy", label:"创建人", minWidth:120},
-				// {prop:"createTime", label:"创建时间", minWidth:120, formatter:this.dateFormat}
-				// {prop:"lastUpdateBy", label:"更新人", minWidth:100},
-				// {prop:"lastUpdateTime", label:"更新时间", minWidth:120, formatter:this.dateFormat}
-			],
+			columns: [],
 			filterColumns: [],
 			pageRequest: { pageNum: 1, pageSize: 10 },
 			pageResult: {},
@@ -255,10 +243,28 @@ export default {
       	handleFilterColumns: function (data) {
 			this.filterColumns = data.filterColumns
 			this.$refs.tableColumnFilterDialog.setDialogVisible(false)
-		  }
+		 },
+		// 处理表格列过滤显示
+      	initColumns: function () {
+			this.columns = [
+				{prop:"id", label:"ID", minWidth:50},
+				{prop:"name", label:"用户名", minWidth:120},
+				{prop:"deptName", label:"机构", minWidth:120},
+				{prop:"roleNames", label:"角色", minWidth:100},
+				{prop:"email", label:"邮箱", minWidth:120},
+				{prop:"mobile", label:"手机", minWidth:100},
+				{prop:"status", label:"状态", minWidth:70},
+				// {prop:"createBy", label:"创建人", minWidth:120},
+				// {prop:"createTime", label:"创建时间", minWidth:120, formatter:this.dateFormat}
+				// {prop:"lastUpdateBy", label:"更新人", minWidth:100},
+				// {prop:"lastUpdateTime", label:"更新时间", minWidth:120, formatter:this.dateFormat}
+			]
+			this.filterColumns = JSON.parse(JSON.stringify(this.columns));
+		  },
 	},
 	mounted() {
 		this.findDeptTree()
+		this.initColumns()
 	}
 }
 </script>
